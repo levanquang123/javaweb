44331683e6644afff0c0464fff2890731efe762f
levanquang123 <levanquang27122005@gmail.com>
2025-05-11 12:07:32 +0700

    first commit

---
+++ src/main/java/com/lap/filter/AuthFilter.java
@@ -0,0 +1,34 @@
+// com.lap.filter.AuthFilter
+package com.lap.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+
+        String loginURI = req.getContextPath() + "/login";
+        boolean loggedIn = session != null && session.getAttribute("user") != null;
+        boolean loginRequest = req.getRequestURI().equals(loginURI);
+
+        if (loggedIn || loginRequest) {
+            chain.doFilter(request, response);
+        } else {
+            res.sendRedirect(loginURI);
+        }
+    }
+
+}
---
+++ src/main/java/com/lap/model/User.java
@@ -0,0 +1,10 @@
+package com.lap.model;
+
+public class User {
+    private String soCCCD;
+    private String email;
+    private String password;
+    private String role; // "SINHVIEN" hoặc "BOGIAODUC"
+
+    
+}
